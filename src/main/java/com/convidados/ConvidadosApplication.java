e5f8d0159a15acaa71c3f0bbf97c19f689973e70
randerson_santana <randerson_santana@estudante.sesisenai.org.br>
2024-04-16 22:15:07 -0300

    Initial commit

---
+++ src/main/java/com/convidados/ConvidadosApplication.java
@@ -0,0 +1,13 @@
+package com.convidados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvidadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvidadosApplication.class, args);
+	}
+
+}
